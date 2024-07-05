216e573f1fa1c94629030998e90903885e9f8978
eduardocd18 <javierecd@gmail.com>
2024-07-05 15:22:49 -0400

    345. Métodos de clases

--- 01.php
+++ 01.php
@@ -10,13 +10,27 @@ class Producto
     public function __construct(public String $nombre, public int $precio, public bool $disponible)
     {
     }
+
+    public function mostarProdcuto()
+    {
+        echo $this->nombre . ". Precio de $" . $this->precio . ".";
+    }
 }
 
 /* -- Instanciar la clase -- */
 $producto = new Producto("Table 7", 40, false);
-echo "<pre>";
-var_dump($producto);
-echo "</pre>";
+$producto->mostarProdcuto();
+
+echo "<br>";
+
+$producto2 = new Producto("Mini PC", 4000, true);
+$producto2->mostarProdcuto();
+
+echo "<br>";
+
+$producto3 = new Producto("UPS", 160, true);
+$producto3->mostarProdcuto();
+
 
 
 include 'includes/footer.php';

4d3710a7073ae88edd2bcacae8e53c3d60e21641
eduardocd18 <javierecd@gmail.com>
2024-07-05 15:46:32 -0400

    346. Modificadores de acceso public y protected

--- 02.php
+++ 02.php
@@ -1,4 +1,50 @@
-<?php include 'includes/header.php';
+<?php
+
+declare(strict_types=1);
+include 'includes/header.php';
+
+/* -- Encapsulación -- */
+class Producto
+{
+    // Constructor
+    public function __construct(protected String $nombre, public int $precio, public bool $disponible)
+    {
+    }
+
+    public function mostarProdcuto(): void
+    {
+        echo $this->nombre . ". Precio de $" . $this->precio . ".";
+    }
+
+    public function getNombre(): string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(String $nombre)
+    {
+        $this->nombre = $nombre;
+    }
+}
+
+/* -- Instanciar la clase -- */
+$producto = new Producto("Tablet 7", 40, false);
+echo $producto->getNombre(); // Obtener el nombre
+$producto->setNombre("PC"); // Modificar el nombre
+echo $producto->getNombre(); // Obterner el nuevo nombre
+
+/* $producto->mostarProdcuto();
+
+echo "<br>";
+
+$producto2 = new Producto("Mini PC", 4000, true);
+$producto2->mostarProdcuto();
+
+echo "<br>";
+
+$producto3 = new Producto("UPS", 160, true);
+$producto3->mostarProdcuto();
+ */
 
 
 include 'includes/footer.php';

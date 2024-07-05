7dac7a5362c8cee5cece84708cdddd9ebb179c57
eduardocd18 <javierecd@gmail.com>
2024-07-05 15:13:28 -0400

    344. Atributos en PHP 8

--- 01.php
+++ 01.php
@@ -6,22 +6,14 @@ include 'includes/header.php';
 /* -- Definir una clase -- */
 class Producto
 {
-    public $nombre;
-    public $precio;
-    public $disponible;
-
     // Constructor
-    public function __construct(String $nombre, int $precio, bool $disponible)
+    public function __construct(public String $nombre, public int $precio, public bool $disponible)
     {
-        $this->nombre = $nombre;
-        $this->precio = $precio;
-        $this->disponible = $disponible;
     }
 }
 
-
 /* -- Instanciar la clase -- */
-$producto = new Producto("Tablet", 40, false);
+$producto = new Producto("Table 7", 40, false);
 echo "<pre>";
 var_dump($producto);
 echo "</pre>";

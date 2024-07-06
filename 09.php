e4d1cfa0b55c4d05ad3615d9d2b94e7ba7abe1b9
eduardocd18 <javierecd@gmail.com>
2024-07-06 16:29:39 -0400

    357. Consultar la Base de Datos con POO y MySQLi

--- 09.php
+++ 09.php
@@ -1,4 +1,32 @@
 <?php include 'includes/header.php';
 
+/* Conectar a la BD con mysqli */
+$db = new mysqli('localhost', 'root', '', 'bienesraices_crud');
+
+// Crear el query
+$query = "SELECT titulo, imagen FROM propiedades";
+
+// Se prepara
+$stmt = $db->prepare($query);
+
+// Se ejecuta
+$stmt->execute();
+
+// Se crea la varible
+$stmt->bind_result($titulo, $imagen);
+
+// Se asigna el resultado
+$stmt->fetch();
+/* 
+var_dump($titulo);
+var_dump($imagen); */
+
+
+while ($stmt->fetch()) :
+    var_dump($titulo);
+    echo "<br>";
+    var_dump($imagen);
+    echo "<br>";
+endwhile;
 
 include 'includes/footer.php';

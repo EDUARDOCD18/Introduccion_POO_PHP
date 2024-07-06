65e6d9eb634aeb8aa528f8f16c53df6f84751e0a
eduardocd18 <javierecd@gmail.com>
2024-07-06 11:20:36 -0400

    353. Namespaces

--- 08.php
+++ 08.php
@@ -1,9 +1,13 @@
 <?php include 'includes/header.php';
 
 /* -- Autoload de clases -- */
+use APP\Clientes;
+use APP\Detalles;
+
 function mi_autoload($clase)
 {
-    require __DIR__ . '/clases/' . $clase . '.php';
+    $partes = explode('\\', $clase);
+    require __DIR__ . '/clases/' . $partes[1] . '.php';
 }
 spl_autoload_register('mi_autoload');
 

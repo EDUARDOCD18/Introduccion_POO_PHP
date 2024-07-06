bb50c5bf9ca2607371d0305035066523771a64bd
eduardocd18 <javierecd@gmail.com>
2024-07-06 10:41:53 -0400

    350. Interfaces

--- 06.php
+++ 06.php
@@ -1,4 +1,26 @@
 <?php include 'includes/header.php';
 
+interface TransporteInterfaz
+{
+    public function getInfo(): string;
+    public function getRuedas(): int;
+}
+
+class Transporte implements TransporteInterfaz
+{
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+    }
+
+    public function getInfo(): string
+    {
+        return "El transporte tiene " . $this->ruedas . " rueda(s). Capacidad de " . $this->capacidad . " asiento(s).";
+    }
+
+    public function getRuedas(): int
+    {
+        return $this->ruedas;
+    }
+}
 
 include 'includes/footer.php';

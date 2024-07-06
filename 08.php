58f53a5a048540f5e075a3daeadb6814f63eba7e
eduardocd18 <javierecd@gmail.com>
2024-07-06 11:10:38 -0400

    352. Autoload de clases

--- 08.php
+++ 08.php
@@ -1,4 +1,14 @@
 <?php include 'includes/header.php';
 
+/* -- Autoload de clases -- */
+function mi_autoload($clase)
+{
+    require __DIR__ . '/clases/' . $clase . '.php';
+}
+spl_autoload_register('mi_autoload');
+
+$detalles = new Detalles();
+echo "<br>";
+$clientes = new Clientes();
 
 include 'includes/footer.php';

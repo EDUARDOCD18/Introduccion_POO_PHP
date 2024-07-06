546d8ebcdd4e721ee4176b9639700756b0b6407c
eduardocd18 <javierecd@gmail.com>
2024-07-06 10:54:59 -0400

    351. Polimorfismo

--- 07.php
+++ 07.php
@@ -1,4 +1,47 @@
 <?php include 'includes/header.php';
 
+interface TransporteInterfaz
+{
+    public function getInfo(): string;
+    public function getRuedas(): int;
+}
+
+abstract class Transporte implements TransporteInterfaz
+{
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+    }
+
+    public function getInfo(): string
+    {
+        return "El transporte tiene " . $this->ruedas . " rueda(s). Capacidad de " . $this->capacidad . " asiento(s).";
+    }
+
+    public function getRuedas(): int
+    {
+        return $this->ruedas;
+    }
+}
+
+class Automovil extends Transporte implements TransporteInterfaz
+{
+    public function __construct(protected int $ruedas, protected int $capacidad, protected string $color)
+    {
+    }
+
+    public function getInfo(): string
+    {
+        return "El transporte tiene " . $this->ruedas . " rueda(s). Capacidad de " . $this->capacidad . " asiento(s). Color: " . $this->color;
+    }
+
+    public function getColor () : string{
+        return "Color: " . $this->color;
+    }
+}
+
+$carro = new Automovil(4, 5, "Verde moco");
+echo $carro->getInfo();
+echo "<br>";
+echo $carro->getColor();
 
 include 'includes/footer.php';

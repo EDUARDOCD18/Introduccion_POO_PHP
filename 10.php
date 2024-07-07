5eab473e336bd6d90efb1de186d4248fad249608
eduardocd18 <javierecd@gmail.com>
2024-07-07 11:17:45 -0400

    358. Consultar Bases de Datos con POO y PDO

--- 10.php
+++ 10.php
@@ -1,4 +1,24 @@
 <?php include 'includes/header.php';
 
+// Conectar a la BD con PDO
+$db = new PDO('mysql:host=localhost; dbname=bienesraices_crud', 'root', '');
+
+// Creamos el query
+$query = "SELECT titulo, imagen from propiedades";
+
+// Consultar la bdd
+$stmt = $db->prepare($query);
+
+// Ejecutar
+$stmt->execute();
+
+// Obtener los resultados
+$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Iterar
+foreach ($resultado as $propiedad) :
+    echo $propiedad['titulo'] . " - " . $propiedad['imagen'] . "<br>";
+
+endforeach;
 
 include 'includes/footer.php';
